3e6088010c30f5ce2356939ffc74e6e9a42aaf5f
calum-macbook <calum.cuthill@ed.ac.uk>
2021-11-25 12:16:00 +0000

    checkpoint

--- GUI/GUI_main.py
+++ GUI/GUI_main.py
@@ -1,7 +1,10 @@
 import sys
 
 from PySide6 import QtGui, QtWidgets
-from PySide6.QtWidgets import QMainWindow, QGridLayout, QApplication, QWidget
+from PySide6.QtCore import QCoreApplication, QRect
+from PySide6.QtWidgets import (QHBoxLayout, QLineEdit, QMainWindow, QGridLayout, QApplication, QWidget,
+    QCheckBox, QVBoxLayout, QFileDialog, QPushButton, QLabel)
+
 from layout_colorwidget import Color
 import resources_rc
 
@@ -13,50 +16,106 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.resize(958, 773)
+        self.generateTab()
+        self.importTab()
+        # self.assignWidgets()
+
         self.show()
-        # self.label_11.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><img src=\":/icons/penguin.png\"/></p></body></html>", None))
 
-        # self.tab = QWidget()
-        # self.widget = QWidget(self.tab)
-        # self.gridLayout_4.addWidget(self.label_7, 0, 0, 1, 1)
+    def generateTab(self):
 
-# class MainWindow(QMainWindow):
+        # New Widget (to be used as a tab)
+        self.ui.tab_x = QWidget()
+        self.ui.tab_x.setObjectName(u"tab_x")
 
-#     def __init__(self):
-#         # super(MainWindow, self).__init__()
-#         super().__init__()
+        # Make a grid layout and position it within the new tab
+        self.ui.gridLayoutWidget_x = QWidget(self.ui.tab_x)
+        self.ui.gridLayoutWidget_x.setObjectName(u"gridLayoutWidget_x")
+        self.ui.gridLayoutWidget_x.setGeometry(QRect(10, 10, 450, 200))
 
-#         self.setWindowTitle("My App")
+        self.ui.gridLayout_x = QGridLayout(self.ui.gridLayoutWidget_x)
+        self.ui.gridLayout_x.setObjectName(u"gridLayout_x")
+        self.ui.gridLayout_x.setContentsMargins(0, 0, 0, 0)
+        self.ui.boxes=[]
+        for i in range(4):
+            self.ui.boxes.append(QCheckBox(self.ui.tab_x))
+            self.ui.gridLayout_x.addWidget(self.ui.boxes[i], i, i, 1, 1)
+            self.ui.boxes[i].toggled.connect(self.checkboxSlot)
 
-#         layout = QGridLayout()
+        # Add tab to the main tab widget, and give it a label
+        self.ui.tabWidget.addTab(self.ui.tab_x, "")
+        self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.ui.tab_x), "abc")
+        
+    def checkboxSlot(self):
+        cbutton = self.sender()
+        print(cbutton.isChecked())
 
-#         layout.addWidget(Color('red'), 0, 0)
-#         layout.addWidget(Color('green'), 1, 0)
-#         layout.addWidget(Color('blue'), 1, 1)
-#         layout.addWidget(Color('purple'), 0, 1)
+    def importTab(self):
 
-#         widget = QWidget()
-#         widget.setLayout(layout)
-#         self.setCentralWidget(widget)
-#         self.show()
+        # New Widget (to be used as a tab)
+        self.ui.tab_import = QWidget()
+        self.ui.tab_import.setObjectName(u"tab_import")
+
+        # Make a Vertical layout within the new tab
+        vbox = QVBoxLayout(self.ui.tab_import)
+
+        label_title = QLabel("Import existing csv/tsv files, extract metadata from filenames")
+        label_input = QLabel("Input Directory:")
+        label_output = QLabel("Output Directory:")
+        self.ui.tbox_input = QLineEdit()
+        self.ui.tbox_output = QLineEdit()
+
+        browse_input = QPushButton("Browse")
+        browse_input.clicked.connect(self.get_input_dir)
+
+        browse_output = QPushButton("Browse")
+        browse_output.clicked.connect(self.get_output_dir)
+
+        hbox_input = QHBoxLayout()
+        hbox_input.addWidget(label_input)
+        hbox_input.addWidget(self.ui.tbox_input)
+        hbox_input.addWidget(browse_input)
+
+        hbox_output = QHBoxLayout()
+        hbox_output.addWidget(label_output)
+        hbox_output.addWidget(self.ui.tbox_output)
+        hbox_output.addWidget(browse_output)
+
+        vbox.addWidget(label_title)
+        vbox.addLayout(hbox_input)
+        vbox.addLayout(hbox_output)
+        vbox.addStretch()
 
 
+    
+        # self.ui.VLayoutWidget_import = QWidget(self.ui.tab_x)
+        # self.ui.VLayoutWidget_import.setObjectName(u"VLayoutWidget_import")
+        # self.ui.VLayoutWidget_import.setGeometry(QRect(10, 10, 450, 200))
 
+        # self.ui.gridLayout_x = QGridLayout(self.ui.VLayoutWidget_import)
+        # self.ui.gridLayout_x.setObjectName(u"gridLayout_x")
+        # self.ui.gridLayout_x.setContentsMargins(0, 0, 0, 0)
+        # self.ui.boxes=[]
+        # for i in range(4):
+        #     self.ui.boxes.append(QCheckBox(self.ui.tab_x))
+        #     self.ui.gridLayout_x.addWidget(self.ui.boxes[i], i, i, 1, 1)
+        #     self.ui.boxes[i].toggled.connect(self.checkboxSlot)
+
+        # Add tab to the main tab widget, and give it a label
+        self.ui.tabWidget.addTab(self.ui.tab_import, "")
+        self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.ui.tab_import), "import")
+        
+    def get_input_dir(self):
+        dirname = QFileDialog.getExistingDirectory(self, "Select Input Directory")
+        self.ui.tbox_input.setText(dirname)
+
+    def get_output_dir(self):
+        dirname = QFileDialog.getExistingDirectory(self, "Select Output Directory")
+        self.ui.tbox_output.setText(dirname)
 
 app = QApplication(sys.argv)
-layout = QGridLayout()
-
-layout.addWidget(Color('red'), 0, 0)
-layout.addWidget(Color('green'), 1, 0)
-layout.addWidget(Color('blue'), 1, 1)
-layout.addWidget(Color('purple'), 0, 1)
-
-widget = QWidget()
-widget.setLayout(layout)
-
-
 w = MainWindow()
-# w.setCentralWidget(widget)
-w.ui.horizontalLayout.addWidget(widget)
 app.setWindowIcon(QtGui.QIcon(":/icons/full-spectrum.png"))
 app.exec()
+
