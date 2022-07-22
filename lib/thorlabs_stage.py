b7848517c4518b2dc74a1aa79ee178ee226ab778
Calum Cuthill <calum.cuthill@gmail.com>
2022-07-22 13:43:32 +0100

    stage in its own class / file

--- lib/thorlabs_stage.py
+++ lib/thorlabs_stage.py
@@ -1,184 +1,208 @@
-import serial
 import time
+import serial
 import serial.tools.list_ports
 import thorlabs_apt_protocol as apt
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
-ports = serial.tools.list_ports.comports()
-ENCODER_POS_COUNTS = 20000 #per mm
-ENCODER_VEL_COUNTS = 204.8 #per mm/s
-
-
-
-
-# msg = apt.mot_move_absolute(source=1, dest=0x50, chan_ident=1, position=2048)
-# port.write(msg)
-# print(f'{msg=}')
-# print(f'ch1 to pos 2048 == {2048/ENCODER_POS_COUNTS}mm')
-
-# host = 0x01
-# ch1 = 0x21
-# ch2 = 0x22
-# ch3 = 0x23
-
-
-class Thor_stage():
+class Thorlabs_Stage():
     def __init__(self):
-        self.pos_x = 0
-        self.pos_y = 0
+        self.encoder_pos_counts = 20000 #per mm
+        self.encoder_vel_counts = 204.8 #per mm/s
+        self.home_pos_x = 55 #mm
+        self.home_pos_y = 37.5 #mm
+
+        self.pos_x = None
+        self.pos_y = None
         self.ref_x = 0
         self.ref_y = 0
-        self.step_x = 0
-        self.step_y = 0
 
-        # self.unpacker = apt.Unpacker(self.port)
+        self.serial_port = None
+        self.port = None
 
-    def list_ports(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.disconnect_serial()
+
+    def scan_serial_ports(self):
         ports = serial.tools.list_ports.comports()
         n =1 
         for port, desc, hwid in sorted(ports):
-            print(f"{n}) {port}: {desc} [{hwid}]")
+            logging.info(f"{n}) {port}: {desc} [{hwid}]")
             n+=1
+        return ports
 
-    def connect_serial(self, port):
-        self.port = serial.Serial(port, 115200, rtscts=True, timeout=0.1)
-        self.port.rts = True
-        self.port.reset_input_buffer()
-        self.port.reset_output_buffer()
-        self.port.rts = False
-        self.port.write(apt.hw_no_flash_programming(source=1, dest=0x21))
-        print('serial should be connected')
-        self.unpacker = apt.Unpacker(self.port)
+    def connect_serial(self, serial_port=None):
+        self.serial_port = serial_port
 
-    def update(self):
-        msg = apt.mot_ack_dcstatusupdate(source=1, dest=0x21)
-        self.port.write(msg)
-        for msg in self.unpacker:
-            print(msg)
+        if self.serial_port:
+            if self.port:
+                self.port.close()
+            self.port = serial.Serial(self.serial_port, 115200, rtscts=True, timeout=0.1)
+            self.port.rts = True
+            self.port.reset_input_buffer()
+            self.port.reset_output_buffer()
+            self.port.rts = False
+            self.port.write(apt.hw_no_flash_programming(source=1, dest=0x21))
+            logging.info(f'thorlabs serial connection on {self.serial_port}')
 
-            # if msgid == status update
-            #     self.pos_x =
-            #     self.pos_y = 
+            self.unpacker = apt.Unpacker(self.port)
+        else:
+            self.port=None
+            logging.warning('thorlabs serial port not specified')
+
+    def disconnect_serial(self):
+        if self.port:
+            logging.info(f'Closing serial port {self.port}')
+            self.port.close()
+        else:
+            logging.info(f'SIMULATING Closing serial port')
+
+    def enable(self):
+        logging.info('Enabling stage channels')
+        self._apt_cmd(apt.mod_set_chanenablestate(source=1, dest=0x21 ,chan_ident=1, enable_state=1))
+        self._apt_cmd(apt.mod_set_chanenablestate(source=1, dest=0x22 ,chan_ident=1, enable_state=1))
+
+    def _apt_cmd(self, cmd_string):
+        logging.debug(f'{cmd_string.hex()=}')
+        if self.port:
+            self.port.write(cmd_string)
+
+    def store_position_reference(self):
+        self.get_position()
+        self.ref_x = self.pos_x
+        self.ref_y = self.pos_y
+
+    def get_position(self):
+        logging.info('getting position')
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x22))
+
+        self.pos_x = None
+        self.pos_y = None
+        while self.pos_x == None or self.pos_y == None:
+            for msg in self.unpacker:
+                if msg.msg == 'mot_get_dcstatusupdate':
+                    if msg.source == 0x21:
+                        self.pos_x = msg.position/self.encoder_pos_counts
+                    if msg.source == 0x22:
+                        self.pos_y = msg.position/self.encoder_pos_counts
+
+        logging.info(f'{self.pos_x=} {self.pos_y=}')
+
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x22))
+
+        # clear unpacker buffer
+        # self.port.reset_input_buffer() #is this better? TODO
+        for msg in self.unpacker: pass
+
+    def _wait_for_home(self):
+        logging.info('waiting for home')
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x22))
+
+        home_complete_x = False
+        home_complete_y = False
+        while home_complete_x == False or home_complete_y == False:
+            for msg in self.unpacker:
+                if msg.msg == 'mot_get_dcstatusupdate':
+                    if msg.source == 0x21 and msg.homed:
+                        home_complete_x = True
+                    if msg.source == 0x22 and msg.homed:
+                        home_complete_y = True
+
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x22))
+        
+        # clear unpacker buffer
+        # self.port.reset_input_buffer() #is this better? TODO
+        for msg in self.unpacker: pass
+
+    def _wait_for_move(self):
+        logging.info('waiting for move')
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_start_updatemsgs(source=1, dest=0x22))
+
+        move_complete_x = False
+        move_complete_y = False
+        while move_complete_x == False or move_complete_y == False:
+            for msg in self.unpacker:
+                # if msg.msg == 'mot_get_dcstatusupdate':
+                    # logging.info(f"{msg.source=} {msg.position=} {msg.homed=} {msg.velocity=} {msg.channel_enabled=}")
+                if msg.msg == 'mot_move_completed':
+                    if msg.source == 0x21:
+                        move_complete_x = True
+                        logging.info('x move complete')
+                    if msg.source == 0x22:
+                        move_complete_y = True
+                        logging.info('y move complete')
+
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x22))
+
+        # clear unpacker buffer
+        # self.port.reset_input_buffer() #is this better? TODO
+        for msg in self.unpacker: pass
+
+    def disable(self):
+        logging.info('Disabling stage')
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x21))
+        self._apt_cmd(apt.hw_stop_updatemsgs(source=1, dest=0x22))
+        self._apt_cmd(apt.mod_set_chanenablestate(source=1, dest=0x21 ,chan_ident=1, enable_state=0))
+        self._apt_cmd(apt.mod_set_chanenablestate(source=1, dest=0x22 ,chan_ident=1, enable_state=0))
 
     def home(self):
-        msg = apt.mot_move_home(source=1, dest=0x21 ,chan_ident=1)
-        self.port.write(msg)
-        print('ch1 moved home')
-        msg = apt.mot_move_home(source=1, dest=0x21 ,chan_ident=2)
-        self.port.write(msg)
-        print('ch2 moved home')
-
-    def set_reference_pos(self, x=None, y=None):
-        if x:
-            self.ref_x = x
+        self._apt_cmd(apt.mot_move_home(source=1, dest=0x21 ,chan_ident=1))
+        self._apt_cmd(apt.mot_move_home(source=1, dest=0x22 ,chan_ident=1))
+        if self.port:
+            logging.info('Homing stage')
+            self._wait_for_home()
         else:
-            self.ref_x = self.pos_x
+            logging.info('SIMULATING Homing stage')
+            time.sleep(6)
+        self.pos_x = self.home_pos_x
+        self.pos_y = self.home_pos_y
 
-        if y:
-            self.ref_y = y
+    def move_abs(self, x, y):
+
+        x_counts = int(x * self.encoder_pos_counts)
+        y_counts = int(y * self.encoder_pos_counts)
+        self._apt_cmd(apt.mot_move_absolute(source=1, dest=0x21, chan_ident=1, position=x_counts))
+        self._apt_cmd(apt.mot_move_absolute(source=1, dest=0x22, chan_ident=1, position=y_counts))
+        if self.port:
+            logging.info(f'moving to position {x},{y}')
+            self._wait_for_move()
         else:
-            self.ref_y = self.pos_y
+            logging.info(f'SIMULATING moving to position {x},{y}')
+            time.sleep(0.5)
+        self.pos_x = x
+        self.pos_y = y
 
-    def move_relative(self, dx, dy):
-
-        print(f'moving relative position dx={dx}mm dy={dy}mm')
-        dx_counts = dx * ENCODER_POS_COUNTS
-        msg = apt.mot_move_relative(source=1, dest=0x50, chan_ident=1, distance=dx_counts)
-        self.port.write(msg)
-
-        dy_counts = dy * ENCODER_POS_COUNTS
-        msg = apt.mot_move_relative(source=1, dest=0x50, chan_ident=2, distance=dy_counts)
-        self.port.write(msg)
-
-    def move_absolute(self, x, y):
-        print(f'moving to position x={x}mm y={y}mm')
-        x_counts = x * ENCODER_POS_COUNTS
-        msg = apt.mot_move_absolute(source=1, dest=0x50, chan_ident=1, position=x_counts)
-        self.port.write(msg)
-        y_counts = y * ENCODER_POS_COUNTS
-        msg = apt.mot_move_absolute(source=1, dest=0x50, chan_ident=2, position=y_counts)
-        self.port.write(msg)
-
+    def move_vs_ref(self, x, y):
+        x_counts = int((self.ref_x + x) * self.encoder_pos_counts)
+        y_counts = int((self.ref_y + y) * self.encoder_pos_counts)
+        self._apt_cmd(apt.mot_move_absolute(source=1, dest=0x21, chan_ident=1, position=x_counts))
+        self._apt_cmd(apt.mot_move_absolute(source=1, dest=0x22, chan_ident=1, position=y_counts))
+        if self.port:
+            logging.info(f'moving to position {x},{y} vs reference {self.ref_x},{self.ref_y}')
+            self._wait_for_move()
+        else:
+            logging.info(f'SIMULATING moving to position {x},{y} vs reference {self.ref_x},{self.ref_y}')
+            time.sleep(0.5)
+        self.pos_x = self.ref_x + x
+        self.pos_y = self.ref_y + y
 
 if __name__ == "__main__":
 
-    stage = Thor_stage()
-    stage.list_ports()
-    stage.connect_serial(port='COM6')
+    # Can use with a context manager (with statement) to ensure serial port gets closed.
+    with Thorlabs_Stage() as stage:
+        stage.scan_serial_ports()
+        stage.connect_serial(serial_port=None)
+        stage.enable()
+        stage.home()
+        stage.move_vs_ref(10,20)
 
-    # stage.move_absolute(5, 5)
-
-
-    while True:
-        print('updating')
-        print(stage.port.read())
-        time.sleep(0.1)
-        stage.update()
-
-
-
-
-
-# def parse_line():
-#     response = bytes('900401002101', 'utf-8')
-#     response = bytes.fromhex('91040E008121010040420F000000000000000000')
-#     response = bytes.fromhex('91040E008121010054D500000000000000000080')
-
-
-#     print(f'{response.hex()=}')
-
-#     msg_id = response[0:2]
-#     print(f'{msg_id=}')
-
-
-#     # packed = pack('lll', 1, 2, 3)
-#     # print(f'{packed=}')
-#     # unpacked = unpack('hhl', packed)
-#     # print(f'{unpacked=}')
-
-#     # response = [response[i:i+2] for i in range(0, len(response), 2)]
-
-#     if msg_id == b'\x44\x04':
-#         chan = response[2:3]
-#         print('ch{chan} has been homed')
-
-#     if msg_id == b'\x91\x04':
-#         chan = response[6:8] #Is this always 0x0100?
-#         position = int(unpack('<l', response[8:12])[0])/ENCODER_POS_COUNTS
-#         velocity = int(unpack('<H', response[12:14])[0])/ENCODER_VEL_COUNTS
-#         status = int(unpack('>I', response[16:20])[0])
-#         print(f'{response[16:20]=}')
-#         print(f'{status=}')
-
-#         status_decode = {
-#         0x00000001 : 'forward hardware limit switch is active',
-#         0x00000002 : 'reverse hardware limit switch is active',
-#         0x00000010 : 'in motion, moving forward',
-#         0x00000020 : 'in motion, moving reverse',
-#         0x00000040 : 'in motion, jogging forward',
-#         0x00000080 : 'in motion, jogging reverse',
-#         0x00000200 : 'in motion, homing',
-#         }
-
-#         print(f'{status=}')
-#         if status in status_decode:
-#             print('success')
-#             print(status_decode[status])
-
-#         print(f'ch{chan} position={position}, velocity={velocity}, status={status}')
-#         print()
-
-
-# def send_status_update_ack(ch):
-#     message_id_1 = 0x92
-#     message_id_2 = 0x04
-#     param1 = 0x01
-#     param2 = 0x00
-#     dest = ch
-#     source = 0x01
-#     output_bytes = bytes([message_id_1, message_id_2, param1, param2, dest, source])
-#     print(f'{output_bytes.hex()=}')
-#     print(f'{(type(output_bytes))=}')
-
-# send_status_update_ack(ch1)
-# parse_line()
