9c7a3d844cf2aff1ff3b3d990a125148d2a417b7
Calum Cuthill <calum.cuthill@gmail.com>
2022-08-04 13:47:20 +0100

    reworked the output options

--- GUI_exportTab.py
+++ GUI_exportTab.py
@@ -2,27 +2,66 @@ import os
 import sys
 from PySide6.QtCore import QObject, QThread, Signal
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLineEdit, QWidget,
-    QVBoxLayout, QFileDialog, QPushButton, QLabel)
+    QVBoxLayout, QFileDialog, QPushButton, QLabel, QComboBox, QProgressBar)
 import logging
 from GUI_dataProcess import DataProcess
 from GUI_tableView import ExportTable
 from GUI_commonWidgets import QHLine, SetupBrowse, MetaFilter
 import lib.csv_helpers as csv
+import pandas as pd
 
 class ExportWorker(QObject):
     finished = Signal()
     progress = Signal(int)
 
-    def __init__(self, setup, meta_df, outfile, data_proc=None):
+    def __init__(self, setup, meta_df, data_proc=None):
         super().__init__()
         self.setup = setup
         self.meta_df = meta_df
-        self.outfile = outfile
-        self.data_proc = data_proc
+        self.outfile = None
+        self.dp = data_proc
 
-    def run(self):
+    def run_spectra(self):
         logging.info(f"running csv.export_dataframes with path={self.setup['datadir']} meta_df=\n{self.meta_df}")
         self.export = csv.export_dataframes(self.setup, self.meta_df, self.outfile, dp=self.data_proc)
+        self.finished.emit()
+
+    def run_stats(self):
+
+        self.export = pd.DataFrame()
+        progress = 0
+        row_num = 0
+        total = len(self.meta_df)
+
+        for row in self.meta_df.index:
+            row_num += 1
+            progress = int(row_num/total * 100)
+            self.progress.emit(progress)
+
+            # locate the datafile and read it in
+            datapath = csv.find_datapath(self.setup, self.meta_df, row)
+            df = pd.read_csv(datapath, sep='\t')
+
+            # Add some metadata columns to the output
+            row_info = pd.DataFrame(index=df.columns[1:])
+            element = self.meta_df.loc[row]['element']
+            surface = self.meta_df.loc[row]['surface']
+            fluid = self.meta_df.loc[row]['fluid']
+            row_info['element'] = element
+            row_info['surface'] = surface
+            row_info['fluid'] = fluid
+            logging.info(f"Processing stats {fluid} {element} {progress}%")
+
+            df = self.dp.process_dataframe(df)
+            stats_df = self.dp.get_stats(df, peak_type='Min')
+            stats_df = pd.concat([row_info, stats_df], axis=1)
+
+            # Accumulate the dataframes in a large 'result' dataframe
+            self.export = pd.concat([self.export, stats_df], axis=0)
+
+        self.export.sort_values(by=['element'], inplace=True)
+        self.export.reset_index(drop=True, inplace=True)
+
         self.finished.emit()
 
 class ExportTab(QWidget):
@@ -31,9 +70,11 @@ class ExportTab(QWidget):
         QWidget.__init__(self)
         self.setObjectName(u"ExportTab")
 
-        # default_outfile = './export.txt'
-        default_outfile = ''
-        default_statsfile = 'stats.txt'
+        default_outfile = 'export.txt'
+        self.export = None
+
+        self.format_options = ["Stats", "LDA Spectra"]
+        self.export_format = self.format_options[0]
 
         # Make a Vertical layout within the new tab
         vbox = QVBoxLayout()
@@ -49,66 +90,47 @@ class ExportTab(QWidget):
         self.tbox_output = QLineEdit()
         self.tbox_output.setText(default_outfile)
 
-        self.tbox_stats = QLineEdit()
-        self.tbox_stats.setText(default_statsfile)
-
         btn_width = 80
         browse_output = QPushButton("Browse")
         browse_output.clicked.connect(self.get_output)
         browse_output.setFixedWidth(btn_width)
 
-        browse_stats = QPushButton("Browse")
-        browse_stats.clicked.connect(self.get_stats_path)
-        browse_stats.setFixedWidth(btn_width)
+        self.btn_save = QPushButton("Save")
+        self.btn_save.clicked.connect(self.save_export)
+        self.btn_save.setFixedWidth(btn_width)
 
-        self.btn_export = QPushButton("Export")
-        self.btn_export.clicked.connect(self.run_export)
-        self.btn_export.setFixedWidth(btn_width)
-
-        self.btn_export_stats = QPushButton("Export")
-        self.btn_export_stats.clicked.connect(self.run_stats)
-        self.btn_export_stats.setFixedWidth(btn_width)
-
-        btn_view_stats= QPushButton("View")
-        btn_view_stats.clicked.connect(self.view_stats)
-        btn_view_stats.setFixedWidth(btn_width)
+        self.btn_apply = QPushButton("Apply")
+        self.btn_apply.clicked.connect(self.run_export)
+        self.btn_apply.setFixedWidth(btn_width)
 
         btn_view_export= QPushButton("View")
         btn_view_export.clicked.connect(self.view_export)
         btn_view_export.setFixedWidth(btn_width)
 
-        hbox_outpath = QHBoxLayout()
-        hbox_outpath.addWidget(QLabel('Export data file:'))
-        hbox_outpath.addWidget(self.tbox_output)
-        hbox_outpath.addWidget(browse_output)
+        self.combo_export = QComboBox()
+        self.combo_export.addItems(self.format_options)
 
-        hbox_statspath = QHBoxLayout()
-        hbox_statspath.addWidget(QLabel('Export stats file:'))
-        hbox_statspath.addWidget(self.tbox_stats)
-        hbox_statspath.addWidget(browse_stats)
+        hbox_outpath = QHBoxLayout()
+        hbox_outpath.addWidget(QLabel('Export file:'))
+        hbox_outpath.addWidget(self.tbox_output, stretch=3)
+        hbox_outpath.addWidget(browse_output)
+        hbox_outpath.addWidget(self.btn_save)
 
         hbox_run = QHBoxLayout()
-        hbox_run.addStretch()
-        hbox_run.addWidget(self.btn_export)
+        hbox_run.addStretch(2)
+        hbox_run.addWidget(QLabel('Format:'))
+        hbox_run.addWidget(self.combo_export, stretch=1)
+        hbox_run.addWidget(self.btn_apply)
         hbox_run.addWidget(btn_view_export)
 
-        hbox_stats = QHBoxLayout()
-        hbox_stats.addStretch()
-        hbox_stats.addWidget(self.btn_export_stats)
-        hbox_stats.addWidget(btn_view_stats)  
-
         vbox_output = QVBoxLayout()
-        vbox_output.addLayout(hbox_outpath)
         vbox_output.addLayout(hbox_run) 
-        vbox_output.addLayout(hbox_statspath)
-        vbox_output.addLayout(hbox_stats)   
+        vbox_output.addLayout(hbox_outpath)
 
         # self.setupBrowse = SetupBrowse()
         self.metaFilter = MetaFilter()
         self.dataProcess = DataProcess()
-        # self.setupBrowse.new_setup.connect(self.metaFilter.setup_changed)
-        # self.setupBrowse.new_setup.connect(self.dataProcess.setup_changed)
-        # self.setupBrowse.update_setup_json()
+        self.dataProcess.request_selection_df.connect(self.metaFilter.select_meta)
         self.metaFilter.add_sel_row()
         self.metaFilter.new_selection_df.connect(self.dataProcess.selection_df_changed)
         self.metaFilter.select_meta()
@@ -149,57 +171,57 @@ class ExportTab(QWidget):
         self.dataProcess.setup_changed(setup)
 
     def get_output(self):
-        outfile, _ = QFileDialog.getSaveFileName(self, "Select Output File:")
+        # outfile, _ = QFileDialog.getSaveFileName(self, "Select Output File:")
+        outfile, _ = QFileDialog.getOpenFileName(self, "Select Output File:")
         self.tbox_output.setText(outfile)
 
-    def get_stats_path(self):
-        statsfile, _ = QFileDialog.getSaveFileName(self, "Select Stats Output File:")
-        self.tbox_stats.setText(statsfile)
+    def save_export(self):
+        outfile = self.tbox_output.text()
+        if self.export is not None:
+            logging.info(f"Writing to {outfile} ...")
+            self.export.to_csv(outfile, sep='\t', na_rep='NA')
+        else:
+            logging.warning(f"Please apply output formatting first")
+
 
     def run_export(self):
-        outfile = self.tbox_output.text()
-
-        if outfile == '':
-            outfile = None
 
         self.thread = QThread()
-
+        self.metaFilter.select_meta()
+        self.dataProcess.apply()
         selection_df = self.metaFilter.selection_df
 
-        self.worker = ExportWorker(self.setup, selection_df, outfile, self.dataProcess.dp)
+        self.worker = ExportWorker(self.setup, selection_df, self.dataProcess.dp)
         self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
+
+        if self.export_format == self.format_options[0]:
+            self.thread.started.connect(self.worker.run_stats)
+        if self.export_format == self.format_options[1]:
+            self.thread.started.connect(self.worker.run_spectra)
+
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
         # Step 6: Start the thread
         self.thread.start()
-        self.btn_export.setEnabled(False)
+        self.btn_apply.setEnabled(False)
         self.thread.finished.connect(self.export_complete)
 
     def export_complete(self):
-        self.btn_export.setEnabled(True)
-        logging.info('Export Complete, Summary:\n')
-        summary = self.worker.export.to_string(max_cols=15, max_rows=15)
-        for line in summary.splitlines():
-            logging.info(line)
-
+        self.btn_apply.setEnabled(True)
+        self.export = self.worker.export
+        # logging.info('Export Complete, Summary:\n')
+        # summary = self.worker.export.to_string(max_cols=15, max_rows=15)
+        # for line in summary.splitlines():
+        #     logging.info(line)
 
     def view_export(self):
         title = os.path.abspath(self.tbox_output.text())
         try:
-            self.table = ExportTable(self.worker.export, title)
+            self.table = ExportTable(self.export, title)
             self.table.show()
         except AttributeError:
             logging.error("Please run export first")
-
-    def run_stats(self):
-        selection_df = self.metaFilter.selection_df
-        self.stats = csv.export_stats(self.setup, self.dataProcess.dp, selection_df, outfile='stats.txt')
-
-    def view_stats(self):
-        self.table_stats = ExportTable(self.stats, 'title')
-        self.table_stats.show()
 
 if __name__ == "__main__":
 
